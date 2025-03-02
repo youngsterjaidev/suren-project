eed7772731551af3967c77e45985cf90001b9607
Jai Dev <47059807+youngsterjaidev@users.noreply.github.com>
2025-03-02 06:40:55 +0000

    resolve the conflict

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const dotenv = require("dotenv")
 const { MongoClient } = require('mongodb');
 const dotenv = require("dotenv")
-
-dotenv.config()
 
 dotenv.config();
 
